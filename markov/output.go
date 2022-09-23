e179af9bc0b37044238ea3b74e498f7b335d3c6c
ActuallyGiggles <95275195+ActuallyGiggles@users.noreply.github.com>
2022-09-23 15:16:40 -0700

    Update output.go

--- markov/output.go
+++ markov/output.go
@@ -1,6 +1,9 @@
 package markov
 
-import "sync"
+import (
+	"sync"
+	wr "github.com/mroth/weightedrand"
+)
 
 var (
 	recursionCounter   = make(map[string]int)
@@ -52,7 +55,66 @@ func outputController(i OutputInstructions, outputC chan result) {
 }
 
 func likelyBeginning(i OutputInstructions, c chan result) {
-	
+	sentence := ""
+	child := ""
+	splitChild := make([]string, 0)
+	nextParent := startKey
+	message := result{
+		Output: "",
+		Problem:   "",
+	}
+
+	chain, exists := jsonToChain("./markovdb/" + channel + ".json")
+	if !exists {
+		message.Output = "We ran into a problem!"
+		message.Problem = "ERROR: " + channel + " does not exist."
+		c <- message
+		close(c)
+		return
+	}
+
+	for true {
+		if list, ok := chain[nextParent]; !ok {
+			message.Output = channel + " has no messages!"
+			message.Problem = "ERROR: " + channel + " has no messages."
+			c <- message
+			close(c)
+			return
+		} else {
+			list := list["nextList"]
+			child = WeightedRandom(list)
+		}
+
+		if child == endPhrase {
+			if len(strings.Split(nextParent, " ")) == 1 {
+				message.Output = sentence + nextParent
+			} else {
+				message.Output = sentence + splitChild[1]
+			}
+			c <- message
+			close(c)
+			return
+		}
+
+		splitChild = strings.Split(child, " ")
+
+		if !DoesSliceContainIndex(splitChild, 1) {
+			if sentence == "" {
+				sentence = child
+			}
+			message.Output = sentence
+			c <- message
+			close(c)
+			return
+		} else {
+			nextParent = child
+			sentence = sentence + splitChild[0] + " "
+		}
+	}
+	message.Output = sentence
+	c <- message
+	close(c)
+	return
 }
 
 func targetedBeginning(i OutputInstructions, c chan result) {
