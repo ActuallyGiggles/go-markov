0a5f52743fe0eead97a30feccf5374a5a575656c
ActuallyGiggles <95275195+ActuallyGiggles@users.noreply.github.com>
2022-12-02 06:24:37 -0800

    Delete api directory

--- api/models.go
+++
@@ -1,9 +0,0 @@
-package api
-
-type APIResponse struct {
-	ModeUsed       string `json:"mode_used"`
-	ChannelUsed    string `json:"channel_used"`
-	MessageUsed    string `json:"message_used"`
-	MarkovSentence string `json:"markov_sentence"`
-	Error          string `json:"error"`
-}
